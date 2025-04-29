603f3487df0e73b4d4d09cdf663360fa8f4dc354
Minatozaki <148259896+MINATOZAKI-PARK@users.noreply.github.com>
2025-04-29 15:50:43 -0600

    first commit

---
+++ src/Runner.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+/**
+ * Clase Runner representa a un corredor con un nombre y velocidad random.
+ * El valor de velocidad es generado como un (int) entre 1 y 30.
+ */
+public class Runner {
+    private String name;
+    private int speed;
+
+    public Runner(String name){
+        Random rand = new Random();
+        this.name = name;
+        this.speed = rand.nextInt(30)+1;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getSeep(){
+        return this.speed;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
